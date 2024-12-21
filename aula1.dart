7cebfdc0d4d139e89c94d13d089ebbf0eda82dd6
Vinícius Matos <vinibmatos@rede.ulbra.br>
2024-12-21 10:54:37 -0300

    2

---
+++ aula1.dart
@@ -0,0 +1,15 @@
+void main() {
+    // 1 - Introducao a Variaveis
+
+    String variavelNome = "Vinícius";
+    print(variavelNome);
+
+    int valorVariavel = 10;
+    print(valorVariavel);
+
+    bool ehVerdadeiro = true;
+    print(ehVerdadeiro);
+
+    List<String> listaDePalavras = ["Vinícius", "Matos"];
+    print('${listaDePalavras[0]} - ${listaDePalavras[1]}');
+}
